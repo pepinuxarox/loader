1577fa7baa41acf773e3e39f0118991185a7aac4
Usuario <Usuario@VALE-FUENTES>
2018-08-27 17:57:27 -0300

    first commit

---
+++ Loader/src/main/java/Model/Entity.java
@@ -0,0 +1,30 @@
+package Model;
+
+public class Entity {
+
+	
+	private int id;
+	private String version;
+	
+	
+	
+	public Entity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	
+	
+}
